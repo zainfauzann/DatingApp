d88911500db3da98565b5d19ec2aad3e9833e7c9
zainfauzann <zainfauzann@gmail.com>
2022-09-14 17:31:21 +0800

    End of section 10

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,28 +9,31 @@ import { AuthGuard } from './_guards/auth.guard';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {
-    path: '',
-    runGuardsAndResolvers: 'always',
-    canActivate: [AuthGuard],
-    children: [
-      {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-      {path: 'members/:username', component: MemberDetailComponent},
-      {path: 'lists', component: ListsComponent},
-      {path: 'messages', component: MessagesComponent},
-    ]
-  },
-  {path: 'errors', component: TestErrorsComponent},
-  {path: 'not-found', component: NotFoundComponent},
-  {path: 'server-error', component: ServerErrorComponent},
-  {path: '**', component: NotFoundComponent, pathMatch: 'full'},
+    {path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+        {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+        {path: 'members/:username', component: MemberDetailComponent},
+        {path: 'member/edit', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
+        {path: 'lists', component: ListsComponent},
+        {path: 'messages', component: MessagesComponent},
+        ]
+    },
+    {path: 'errors', component: TestErrorsComponent},
+    {path: 'not-found', component: NotFoundComponent},
+    {path: 'server-error', component: ServerErrorComponent},
+    {path: '**', component: NotFoundComponent, pathMatch: 'full'},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
